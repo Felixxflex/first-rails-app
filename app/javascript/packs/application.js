22580d139905ccb41818d64a6d51bb6a6b70e897
Felix Hermann <felixhermann@MacBook-Pro-2.local>
2020-09-05 22:48:45 +0200

    started the whole landingpage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,68 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
+require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+
+// ----------------------------------------------------
+// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+// WRITE YOUR OWN JS STARTING FROM HERE 👇
+// ----------------------------------------------------
+
+// External imports
+import "bootstrap";
+import { mobileMenuNav} from '../components/navbar'
+import { initAutocomplete } from '../plugins/init_autocomplete'
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  mobileMenuNav();
+  initAutocomplete();
+  var select = function(s) {
+    return document.querySelector(s);
+  }
+  function randomBetween(min,max)
+  {
+      var number = Math.floor(Math.random()*(max-min+1)+min);
+    
+      if ( number !== 0 ){
+        return number;
+      }else {
+        return 0.5;
+      }
+  }
+  var tl = new TimelineMax();
+  
+  for(var i = 0; i < 20; i++){
+  
+    var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+      x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+      y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
+      repeat:-1,
+      repeatDelay:randomBetween(0.2, 0.5),
+      yoyo:true,
+      ease:Elastic.easeOut.config(1, 0.5)
+    })
+  
+    tl.add(t, (i+1)/0.6)
+  }
+  
+  tl.seek(50);
+  // initMapbox();
+});
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
