f9f497cc92040ccfb748d2962c942a488b8bdb6c
Felix Hermann <felixhermann@Felixs-Air.fritz.box>
2020-09-24 16:00:46 +0200

    some changes and finished lot of sites

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/path/web_development/page", to: "pages#web_development", as: "web_development"
+  get "/path/web_app/page", to: "pages#web_app", as: "web_app"
+  get "/path/social_media/page", to: "pages#social_media", as: "social_media"
+  get "/path/about/page", to: "pages#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
