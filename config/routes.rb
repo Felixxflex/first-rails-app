4124032bc74fa92a505c6e9b5305aae87bb9f65b
Felix Hermann <felixhermann@Felixs-Air.fritz.box>
2020-09-25 17:11:53 +0200

    new changes 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "/path/web_app/page", to: "pages#web_app", as: "web_app"
   get "/path/social_media/page", to: "pages#social_media", as: "social_media"
   get "/path/about/page", to: "pages#about", as: "about"
+  get "/path/benefits/page", to: "pages#benefits", as: "benefits"
+  get "/path/contact/page", to: "pages#contact", as: "contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
