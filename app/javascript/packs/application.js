b3454f61c18f07fd5db273f8662d2946cb9668d1
Felix Hermann <felixhermann@Felixs-Air.fritz.box>
2020-10-05 12:16:26 +0200

    packs java

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,55 +29,9 @@ import { mobileMenuNav } from '../components/navbar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-new webpack.ProvidePlugin({ // inject ES5 modules as global vars
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Tether: 'tether'
-})
-
 document.addEventListener('turbolinks:load', () => {
     // Call your functions here, e.g:
     // initSelect2();
     mobileMenuNav();
-    initAutocomplete();
-    var select = function(s) {
-        return document.querySelector(s);
-    }
 
-    function randomBetween(min, max) {
-        var number = Math.floor(Math.random() * (max - min + 1) + min);
-
-        if (number !== 0) {
-            return number;
-        } else {
-            return 0.5;
-        }
-    }
-    var tl = new TimelineMax();
-
-    for (var i = 0; i < 20; i++) {
-
-        var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-            x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-            y: randomBetween(12, 15) * (randomBetween(-1, 1)),
-            repeat: -1,
-            repeatDelay: randomBetween(0.2, 0.5),
-            yoyo: true,
-            ease: Elastic.easeOut.config(1, 0.5)
-        })
-
-        tl.add(t, (i + 1) / 0.6)
-    }
-
-    tl.seek(50);
-    // initMapbox();
 });
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
