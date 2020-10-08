6a47d19a80017471a49131068676ce0faa714340
Felix Hermann <17felix2000@gmail.com>
2020-10-08 11:48:15 +0200

    fixxin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
+};
