ec41386f96d43b36170cdaa3ae332b0ec9684e5f
Felix Hermann <17felix2000@gmail.com>
2020-10-08 17:49:03 +0200

    fixxes

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -13,3 +13,20 @@ module.exports = {
         mode: 'development'
     },
 };
+var path = require('path');
+
+module.exports = {
+    entry: './main.ts',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
