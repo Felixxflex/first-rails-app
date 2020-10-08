65bd30f9fa0fc6fe3cc2e952dc6a9d432c4e566d
Felix Hermann <17felix2000@gmail.com>
2020-10-08 19:02:56 +0200

    problem fix

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,7 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-const common = require('./webpack.common.js')();
-
-module.exports = merge(common, {});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack');
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+    })
+);
 module.exports = environment
