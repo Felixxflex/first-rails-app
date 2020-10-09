8d4b585a312131213f8acadce2ac872e94dc5f8c
Felix Hermann <17felix2000@gmail.com>
2020-10-09 15:20:49 +0200

    weird fix

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module FirstRailsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.initialize_on_precompile = false
+    config.secret_key_base = 'Jordansfelix20001710'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
