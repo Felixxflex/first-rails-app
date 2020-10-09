7a1cd03f2f3b63a399fad6a36b6947a264797eb3
Felix Hermann <17felix2000@gmail.com>
2020-10-09 13:50:06 +0200

    entry

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -20,3 +20,20 @@ module.exports = {
     },
 };
 var path = require('path');
+
+
+module.exports = {
+    entry: './main.ts',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+};
